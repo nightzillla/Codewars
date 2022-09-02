53dc196484dc3dd7c69f28772b606c5c82d0dd68
nightzillla <81122802+nightzillla@users.noreply.github.com>
2022-09-02 15:58:52 -0500

    working on codewars

---
+++ 6-Kyu.js
@@ -0,0 +1,24 @@
+// 6 kyu: Simple Fun #52: Pairs of shoes
+let shoes = [
+    [0, 21],
+    [1, 23],
+    [1, 21],
+    [0, 23]
+];
+
+function pairOfShoes(shoes) {
+        let shoesHasMatch = false;
+        let shoeThatMatches;
+        let newArray;
+    for (let i = 1; i < shoes.length; i++) {
+        if (shoes[i][1] === shoes [0][1]) {
+            if (shoes[i][0] === shoes[0][0]) {
+                shoesHasMatch = true;
+                shoes.splice(i, 1);
+                shoes.splice(0, 1);
+                return pairOfShoes(shoes);
+            }
+        }
+    }
+    return shoesHasMatch;
+}
